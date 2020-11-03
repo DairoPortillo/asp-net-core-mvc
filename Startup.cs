9c224530d365e5dcd1452a02d6c1e4134a1ffbd1
Dairo Portillo <dairo.jose17@hotmail.com>
2020-11-02 22:57:44 -0500

    Controller inicial

--- Startup.cs
+++ Startup.cs
@@ -50,7 +50,7 @@ namespace asp_net_core_mvc
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                    pattern: "{controller=Escuela}/{action=Index}/{id?}");
             });
         }
     }
